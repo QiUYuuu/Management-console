12e377cf4a0a2c6213d0f5c4db559619befc0c56
QiUYuuu <41138622+QiUYuuu@users.noreply.github.com>
2019-05-17 20:08:22 +0800

    新增密钥和数据库地址

---
+++ mmc/config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secretOrKey: "secret",
+  mongoURI: "mongodb+srv://chenxi:chenxi1112@cluster0-y6ipn.mongodb.net/test?retryWrites=true"
+  //mongoURI: "mongodb://localhost/a"
+};
