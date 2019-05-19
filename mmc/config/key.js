71e2cc86d1108b7c21a3d92c6ce1a49306729d80
QiUYuuu <41138622+QiUYuuu@users.noreply.github.com>
2019-05-19 10:43:45 +0800

    把网络数据库暂时换成本地数据库

--- mmc/config/key.js
+++ mmc/config/key.js
@@ -1,5 +1,5 @@
 module.exports = {
   secretOrKey: "secret",
-  mongoURI: "mongodb+srv://chenxi:chenxi1112@cluster0-y6ipn.mongodb.net/test?retryWrites=true"
-  //mongoURI: "mongodb://localhost/a"
+  //mongoURI: "mongodb+srv://chenxi:chenxi1112@cluster0-y6ipn.mongodb.net/test?retryWrites=true"
+  mongoURI: "mongodb://localhost:3000/a"
 };
