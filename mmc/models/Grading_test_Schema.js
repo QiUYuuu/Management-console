861419fe033962df5edb1f357e76aa0bfa14eed8
QiUYuuu <41138622+QiUYuuu@users.noreply.github.com>
2019-05-19 10:02:56 +0800

    新增考试分数的数据库

---
+++ mmc/models/Grading_test_Schema.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Creat Schema
+//考试分数的数据库
+const TestSchema = new Schema({
+  //学生姓名
+  StudentName: {
+    type: String,
+    required: true
+  },
+  //学生学号
+  Student_ID: {
+    type: Number,
+    required: true
+  },
+  //考级科目
+  testName: {
+    type: String,
+    required: true
+  },
+  //考试分数
+  score: {
+    type: Number,
+    required: true
+  },
+  //考级等级
+  test_level: {
+    type: String,
+    required: true
+  }
+});
+
+const test = mongoose.model("classinfo",TestSchema);
+
+module.exports = test;
