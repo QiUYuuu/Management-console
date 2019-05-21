4d77d6b22ec93bc4fe4c7123a6bfe6b28088f8c9
QiUYuuu <41138622+QiUYuuu@users.noreply.github.com>
2019-05-21 11:26:19 +0800

    新加登陆模板

--- mmc/src/main.js
+++ mmc/src/main.js
@@ -2,19 +2,25 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 import VueRouter from 'vue-router'
+//import  DB from '../config/key'
 
 import  Index from './components/index'
+import  Login from './components/login'
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+
+//const db = DB.mongoURI;
 
 
 const router = new VueRouter({
   mode: "history",
   base: __dirname,
   routes:[
-    {path:"/",component:Index}
+    {path:"/",component:Login},
+    {path:"/index",component:Index}
   ]
 });
 
