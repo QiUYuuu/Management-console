3bc4395bd6d2a60cbefa5ccd072a30818c9653ae
QiUYuuu <41138622+QiUYuuu@users.noreply.github.com>
2019-05-29 20:01:52 +0800

    培养方案组件

---
+++ mmc/src/components/Training_Program/Training_Program.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="Training_Program">
+    培养方案
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Training_Program"
+    }
+</script>
+
+<style scoped>
+
+</style>
