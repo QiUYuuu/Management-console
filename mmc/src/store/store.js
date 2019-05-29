e04cd80b6ed6f550ae566124819cdc2f679e592b
QiUYuuu <41138622+QiUYuuu@users.noreply.github.com>
2019-05-29 20:02:13 +0800

    vuex改动

--- mmc/src/store/store.js
+++ mmc/src/store/store.js
@@ -4,23 +4,32 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 export const store = new Vuex.Store({
   state:{
+    //token用来储存登陆用来验证的token
+    //userInfo用来储存登陆用户的信息
     user: {}
   },
   getters: {
-    addToken(response){
-      state.user.token = response;
+    token: (state) => {
+      return state.user.token;
+    },
+    personalInfo: (state) => {
+      return state.user.userInfo;
     }
   },
   mutations:{
-    addToken(state,response){
-      state.user.token = response;
+    addToken(state,payload){
+      state.user.token = payload;
+    },
+    addUserInfo: (state,payload) => {
+      state.user.userInfo = payload;
     }
   },
   actions: {
-    reducePrice: (context,payload) => {
-      setTimeout(function(){
-        context.commit("reducePrice",payload);
-      },2000);
+    addToken: (context,payload) => {
+      context.commit("addToken",payload);
+    },
+    addUserInfo: (context,payload) => {
+      context.commit("addUserInfo",payload);
     }
   }
 });
